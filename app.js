68baecfd0b1ceb31ad16cd0653a857c5c6d29e2c
DevRafaelCena <rafael.cena@hotmail.com>
2020-02-16 11:57:15 -0300

    rotas 2

--- app.js
+++ app.js
@@ -8,18 +8,15 @@ const produto = {
 }
 
 app.post("/produto/criar",(req,res)=> {
-     produto.tipoProduto="living",produto.preco=1245,produto.quantidade=300
+    // produto.tipoProduto="living",produto.preco=1245,produto.quantidade=300
      res.send(produto)
  })
- app.get("/produto/criar",(req,res)=> {
-    // produto.tipoProduto="living",produto.preco=1245,produto.quantidade=300
-     res.send(app.post("/produto/criar",(req,res)=> {
-        produto.tipoProduto="living",produto.preco=1245,produto.quantidade=300
-        res.send(produto)
-    }))
+ //rota dinamica  ("/produtos/:id?" fica opcional informar o id
+ app.get("/produtos/:id",(req,res)=>{
+     let {id} = req.params
+     console.log("eu tenho um produto com o id: ", id)
+     res.send("eu tenho um produto com o id: " + id)
  })
- 
- 
 app.get('/',(req,res)=> res.send("Ola mundo!!!"))
 app.get('/contatos',(req,res)=> res.send("Contatossssss"))
 app.listen(3000,()=>{
